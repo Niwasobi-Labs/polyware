e2f0ed85257ded3907448f26d48cbbda3f3edc53
Spencer Peterson <spendrake@icloud.com>
2025-05-21 13:41:20 -0700

    Telemetry Documentation Update

--- Telemetry/ITelemetryEvent.cs
+++ Telemetry/ITelemetryEvent.cs
@@ -3,7 +3,7 @@ using System.Text;
 namespace PolyWare.Telemetry {
 	public interface ITelemetryEvent {
 		/// <summary>
-		/// EventIDs must be unique and have a limit of 10000
+		/// EventIDs must be unique and have a limit of 10000 for game specific events
 		/// </summary>
 		public int EventID { get; }
 		public string EventName { get; }
