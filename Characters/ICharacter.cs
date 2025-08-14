219a23512b78e3ee3d1ac43e55933db253b5f2c0
Spencer Peterson <spendrake@icloud.com>
2025-08-14 10:54:37 -0700

    Adding Transform Gettor to ICharacter

--- Characters/ICharacter.cs
+++ Characters/ICharacter.cs
@@ -1,9 +1,11 @@
 using PolyWare.Interactions;
 using PolyWare.Items;
+using UnityEngine;
 
 namespace PolyWare.Characters {
 	public interface ICharacter {
 		public void Interact();
 		public bool Pickup(IPickupable item);
+		public Transform Transform { get; }
 	}
 }
