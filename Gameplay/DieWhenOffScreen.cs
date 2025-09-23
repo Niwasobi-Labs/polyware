fac347c467e4c14342815a546422a9611614cdba
Spencer Peterson <spendrake@icloud.com>
2025-09-23 16:45:27 -0700

    Added DieWhenOffScreen component

---
+++ Gameplay/DieWhenOffScreen.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+namespace PolyWare.Gameplay {
+	// must be put alongside a renderer
+	public class DieWhenOffScreen : MonoBehaviour {
+		[SerializeField] private GameObject target;
+		[SerializeField] private float killTimer;
+		
+		private void OnBecameInvisible() {
+			if (killTimer > 0) StartCoroutine(KillTimer());
+			else Kill();
+		}
+
+		private IEnumerator KillTimer() {
+			yield return Utils.Yielders.WaitForSeconds(killTimer);
+			
+			Kill();
+		}
+
+		private void Kill() {
+			Destroy(target);
+		}
+	}
+}
