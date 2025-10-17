7f7c47e4d40578cdca70625d7e88c5c4dae1077f
Spencer Peterson <spendrake@icloud.com>
2025-10-17 13:09:25 -0700

    Added Damage and Luck stat

--- Stats/StatData.cs
+++ Stats/StatData.cs
@@ -6,6 +6,8 @@ namespace PolyWare.Stats {
 		Speed,
 		Dexterity,
 		Vitality,
+		Damage,
+		Luck,
 	}
 	
 	[Serializable]
@@ -14,6 +16,8 @@ namespace PolyWare.Stats {
 		public float Speed = 1;
 		public float Dexterity = 1;
 		public float Vitality = 1;
+		public float Damage = 1;	
+		public float Luck = 1;
 		
 		public float this[StatType statType] {
 			get {
@@ -22,6 +26,8 @@ namespace PolyWare.Stats {
 					StatType.Speed => Speed,
 					StatType.Dexterity => Dexterity,
 					StatType.Vitality => Vitality,
+					StatType.Damage => Damage,
+					StatType.Luck => Luck,
 					_ => throw new ArgumentOutOfRangeException(nameof(statType), statType, null)
 				};
 			}
