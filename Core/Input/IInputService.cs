9565ded303e29a20da45b2cad198a86f7722d4fd
Spencer Peterson <spendrake@icloud.com>
2025-11-24 12:42:09 -0800

    Added player/ui action map toggles to InputSystem
    - Related to work for $1aa

--- Core/Input/IInputService.cs
+++ Core/Input/IInputService.cs
@@ -1,9 +1,13 @@
 namespace PolyWare.Core {
 	public interface IInputService : IService {
 		void ChangeToActionMap(int actionMap);
+		void TogglePlayerInput(bool status);
+		void ToggleUIInput(bool status);
 	}
 	
 	public class NullInputService : IInputService {
 		public void ChangeToActionMap(int actionMap) {}
+		public void TogglePlayerInput(bool status) {}
+		public void ToggleUIInput(bool status) {}
 	}
 }
