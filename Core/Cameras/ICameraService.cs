de4f83a2f3b05632e784b247a4d9028fd6b6ce08
Spencer Peterson <spendrake@icloud.com>
2025-12-02 09:49:26 -0800

    added zoom functionality to ICameraService
    - related to $1ju

--- Core/Cameras/ICameraService.cs
+++ Core/Cameras/ICameraService.cs
@@ -5,6 +5,8 @@ namespace PolyWare.Core {
 		public Camera ActiveCamera { get; }
 		public void SetFollowTarget(Transform target);
 		public void SetBoundingVolume(Collider volume);
+		public void SetZoom(float zoom, bool instant = false);
+		public void ResetZoom(bool instant = false);
 		public void FollowPlayer();
 		public void Shake(CameraShakeSettings shakeSettings);
 	}
