5324d9f6b1442751f50ee93e05d8edf48b0631f2
Spencer Peterson <spendrake@icloud.com>
2025-12-04 13:52:02 -0800

    made SpawnEntityManager easier to use
    - automatically grabs children instead of manually adding them

--- Game/Spawning/SimpleSpawnEntityManager.cs
+++ Game/Spawning/SimpleSpawnEntityManager.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 namespace PolyWare.Game {
 	public class SimpleSpawnEntityManager : MonoBehaviour {
-		[SerializeField] private List<SpawnEntity> spawnEntities;
+		private readonly List<SpawnEntity> spawnEntities = new List<SpawnEntity>();
+
+		private void FindSpawners() {
+			GetComponentsInChildren<SpawnEntity>(true, spawnEntities);
+		}
 
 		public void Spawn() {
+			FindSpawners();
+			
 			for (int i = 0; i < spawnEntities.Count; i++) {
-				spawnEntities[i].Spawn();
+				spawnEntities[i]?.Spawn();
 			}
 		}
 	}
