fbcc099c6ee18b7cd12156e7e9d352b9f9d59c46
Spencer Peterson <spendrake@icloud.com>
2025-12-04 15:05:09 -0800

    added ItemPickedUp event for use

--- Game/Items/ItemDefinition.cs
+++ Game/Items/ItemDefinition.cs
@@ -1,8 +1,15 @@
+using PolyWare.Core;
+
 namespace PolyWare.Game {
+	public struct ItemPickedUpEvent : IEvent {
+		public ItemDefinition ItemDefinition;
+	}
+	
 	public abstract class ItemDefinition : EntityDefinition {
 		public string ItemID; // todo: make an enum of sorts
 		public string Name;
 		public string Description;
+		public string PickupMessage;
 		public int MaxStack = 1;
 	}
 }
