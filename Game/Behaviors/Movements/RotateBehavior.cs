238be0f3f243286d5e09512d91e5922076b9d686
Spencer Peterson <spendrake@icloud.com>
2025-12-05 12:22:32 -0800

    added changeDirection timer to RotateBehavior

--- Game/Behaviors/Movements/RotateBehavior.cs
+++ Game/Behaviors/Movements/RotateBehavior.cs
@@ -1,4 +1,5 @@
 using System;
+using PolyWare.Core;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -7,6 +8,7 @@ namespace PolyWare.Game {
 	[Serializable]
 	public class RotateBehaviorFactory : MoveBehaviorFactory {
 		public bool RandomRotation = true;
+		public int ChangeDirectionsAfter = -1;
 		[HideIf("RandomRotation")] public Vector3 RotateAround = Vector3.up;
 		
 		public override IBehavior Create(ICharacter parent) {
@@ -17,12 +19,25 @@ namespace PolyWare.Game {
 	public class RotateBehavior : MoveBehavior {
 		private Vector3 rotationAxis;
 
+		private CountdownTimer changeDirectionTimer;
+		private float directionModifier;
+		
 		public RotateBehavior(ICharacter character, RotateBehaviorFactory factory) : base(character, factory) {
 			rotationAxis = factory.RandomRotation ? Random.rotation.eulerAngles : factory.RotateAround;
+			directionModifier = 1;
+			
+			if (factory.ChangeDirectionsAfter > 0) {
+				changeDirectionTimer = new CountdownTimer(factory.ChangeDirectionsAfter);
+				changeDirectionTimer.OnTimerComplete += () => {
+					directionModifier *= -1;
+					changeDirectionTimer.Restart();
+				};
+				
+			}
 		}
 		
 		protected override void OnStart() {
-			// noop
+			changeDirectionTimer?.Start();
 		}
 
 		public override void HitWall(Vector3 hitNormal) {
@@ -30,7 +45,8 @@ namespace PolyWare.Game {
 		}
 
 		protected override void OnTick(float dt) {
-			parent.Transform.Rotate(rotationAxis, parent.MoveSettings.TurnSpeed * dt);
+			parent.Transform.Rotate(rotationAxis, (parent.MoveSettings.TurnSpeed * directionModifier) * dt);
+			changeDirectionTimer?.Tick(dt);
 		}
 		
 		protected override void OnComplete() {
