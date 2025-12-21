6096b3cf86d7c8359b1c426abf419204ef294ab7
Spencer Peterson <spendrake@icloud.com>
2025-12-21 15:22:48 -0800

    more formatting

--- Core/UI/Widgets/Widget.cs
+++ Core/UI/Widgets/Widget.cs
@@ -1,23 +1,27 @@
 using System;
 using UnityEngine;
 
-namespace PolyWare.Core {
+namespace PolyWare.Core
+{
 	[RequireComponent(typeof(RectTransform))]
-	public class Widget : MonoBehaviour {
+	public class Widget : MonoBehaviour
+	{
 		[SerializeField] private GameObject visualParentOverride;
 		[SerializeField] protected WidgetTweenAnimationContext tweenAnimationContext;
 
 		public event Action OnCloseAnimationComplete;
 
 		protected GameObject VisualParent => visualParentOverride ? visualParentOverride : gameObject;
-		
 
-		public virtual void Open() {
+		public virtual void Open()
+		{
 			tweenAnimationContext.StartOpenAnimation(VisualParent.transform);
 		}
 
-		public virtual void Close() {
-			tweenAnimationContext.StartCloseAnimation(VisualParent.transform, () => {
+		public virtual void Close()
+		{
+			tweenAnimationContext.StartCloseAnimation(VisualParent.transform, () =>
+			{
 				VisualParent.SetActive(false);
 				OnCloseAnimationComplete?.Invoke();
 			});
